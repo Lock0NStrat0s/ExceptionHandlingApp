4098f00375a10492333b950e4f9fe712af16c237
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-05-12 11:51:25 -0600

    code complete.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,5 +2,13 @@
 int[] ages = new int[] { 1, 3, 5 };
 for (int i = 0; i <= ages.Length; i++)
 {
-     Console.WriteLine(ages[i]);
+	try
+	{
+        Console.WriteLine(ages[i]);
+    }
+    catch (Exception ex)
+	{
+        Console.WriteLine(ex.Message);
+        throw new Exception("There was an error handling the array", ex);
+    }
 }
